f4c8ea9d8c18a299f40a8cd6489db14d8bf1259a
TristanTA <all20052@byui.edu>
2025-09-12 17:55:46 -0600

    first kt strucutre

---
+++ MainActivity.kt
@@ -0,0 +1,9 @@
+# Imports
+val passmode = com.miralens.Modes.PassiveMode()
+
+# Toggle
+
+# Mode Selection
+# - Options: Passive, Ambient, Exploration
+
+passmode.PassiveMode()
---
+++ Modes/PassiveMode.kt
@@ -0,0 +1,7 @@
+package com.miralens.Modes
+
+class PassiveMode {
+    fun PassiveMode() {
+        println("Passive Mode Activated")
+    }
+}
